d4c14390173851b0132b3061fb126e1899549738
Chas Rickarby <chas@3playmedia.com>
2019-06-13 22:15:41 -0400

    Most common firstnames is done

--- actor_parser.rb
+++ actor_parser.rb
@@ -5,15 +5,36 @@ wholenames = {}
 firstname_c = 0
 lastname_c = 0
 wholename_c = 0
+common_firstnames = []
+common_lastnames = []
 
-IO.foreach(file).lazy.reject{ |l| l[0..3] == "    " }.each do |line|
+
+IO.foreach(file).lazy.reject{ |l| l[0..3] == "    " }.first(1000).each do |line|
   wholename = line.split(" --")[0]
   lastname, firstname = wholename.split(', ')
-  firstnames[firstname] = true && firstname_c += 1 if firstnames[firstname].nil?
-  lastnames[lastname] = true && lastname_c += 1 if lastnames[lastname].nil?
-  wholenames[wholename] = true && wholename_c += 1 if wholenames[wholename].nil?
+
+  puts firstnames[firstname]
+  if firstnames[firstname].nil?
+    firstnames[firstname] = 1
+    firstname_c += 1
+  else
+    firstnames[firstname] = 1 + firstnames[firstname]
+  end
+
+  lastnames[lastname].nil? ? lastnames[lastname] = 1 && lastname_c += 1 : lastnames[lastname] += 1
+  wholenames[wholename].nil? ? wholenames[wholename] = 1 && wholename_c += 1 : wholenames[wholename] += 1
+
+  if common_firstnames.count < 10
+    common_firstnames.push({firstname.to_s => firstnames[firstname]})
+  else
+    if common_firstnames.last.values.first < firstnames[firstname]
+      common_firstnames[0] = {firstname.to_s => firstnames[firstname]}
+      common_firstnames = common_firstnames.sort_by { |k| k.values.first }
+    end
+  end
 end
 
 puts "Unique firstnames: #{firstname_c}"
 puts "Unique lastnames: #{lastname_c}"
 puts "Uniuqe wholenames: #{wholename_c}"
+puts "Most common firstnames: #{common_firstnames}"
