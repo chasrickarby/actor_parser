c7bf2f41a4f702a2c72612cf2e0a42343b6d45b1
Chas Rickarby <chas@3playmedia.com>
2019-06-13 22:55:37 -0400

    Most common first and last names are both working

--- actor_parser.rb
+++ actor_parser.rb
@@ -5,15 +5,14 @@ wholenames = {}
 firstname_c = 0
 lastname_c = 0
 wholename_c = 0
-common_firstnames = []
-common_lastnames = []
+common_firstnames = {}
+common_lastnames = {}
 
 
-IO.foreach(file).lazy.reject{ |l| l[0..3] == "    " }.first(1000).each do |line|
+IO.foreach(file).lazy.reject{ |l| l[0..3] == "    " }.each do |line|
   wholename = line.split(" --")[0]
   lastname, firstname = wholename.split(', ')
 
-  puts firstnames[firstname]
   if firstnames[firstname].nil?
     firstnames[firstname] = 1
     firstname_c += 1
@@ -21,16 +20,44 @@ IO.foreach(file).lazy.reject{ |l| l[0..3] == "    " }.first(1000).each do |line|
     firstnames[firstname] = 1 + firstnames[firstname]
   end
 
-  lastnames[lastname].nil? ? lastnames[lastname] = 1 && lastname_c += 1 : lastnames[lastname] += 1
-  wholenames[wholename].nil? ? wholenames[wholename] = 1 && wholename_c += 1 : wholenames[wholename] += 1
+  if lastnames[lastname].nil?
+    lastnames[lastname] = 1
+    lastname_c += 1
+  else
+    lastnames[lastname] = 1 + lastnames[lastname]
+  end
+
+  if wholenames[wholename].nil?
+    wholenames[wholename] = 1
+    wholename_c += 1
+  else
+    wholenames[wholename] = 1 + wholenames[wholename]
+  end
 
   if common_firstnames.count < 10
-    common_firstnames.push({firstname.to_s => firstnames[firstname]})
-  else
-    if common_firstnames.last.values.first < firstnames[firstname]
-      common_firstnames[0] = {firstname.to_s => firstnames[firstname]}
-      common_firstnames = common_firstnames.sort_by { |k| k.values.first }
+    common_firstnames[firstname.to_s] = firstnames[firstname]
+    common_firstnames = common_firstnames.sort_by {|k, v| -v}.to_h
+  elsif common_firstnames[common_firstnames.keys.last] < firstnames[firstname]
+    unless common_firstnames[firstname].nil?
+      common_firstnames[firstname] = firstnames[firstname]
+    else
+      common_firstnames.delete(common_firstnames.keys.last)
+      common_firstnames[firstname] = firstnames[firstname]
     end
+    common_firstnames = common_firstnames.sort_by {|k, v| -v}.to_h
+  end
+
+  if common_lastnames.count < 10
+    common_lastnames[lastname.to_s] = lastnames[lastname]
+    common_lastnames = common_lastnames.sort_by {|k, v| -v}.to_h
+  elsif common_lastnames[common_lastnames.keys.last] < lastnames[lastname]
+    unless common_lastnames[lastname].nil?
+      common_lastnames[lastname] = lastnames[lastname]
+    else
+      common_lastnames.delete(common_lastnames.keys.last)
+      common_lastnames[lastname] = lastnames[lastname]
+    end
+    common_lastnames = common_lastnames.sort_by {|k, v| -v}.to_h
   end
 end
 
@@ -38,3 +65,4 @@ puts "Unique firstnames: #{firstname_c}"
 puts "Unique lastnames: #{lastname_c}"
 puts "Uniuqe wholenames: #{wholename_c}"
 puts "Most common firstnames: #{common_firstnames}"
+puts "Most common lastnames: #{common_lastnames}"
